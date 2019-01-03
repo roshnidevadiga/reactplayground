c6fa1c1dd3f6d5257313a0d8b7424a395f4a4feb
bhagyashree <bhagyashree@druva.com>
2019-01-03 10:54:41 +0530

    added code for reading files from directory

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {
+    basePath: './src/components',
+    cssPath: './src/css',
+    outputPath: './src/filePaths.js'
+};
+
+module.exports = config;
---
+++ initial.js
@@ -0,0 +1,29 @@
+const config = require('./config');
+const { lstatSync, readdirSync, writeFile } = require('fs');
+const { join } = require('path');
+
+const isDirectory = source => lstatSync(source).isDirectory();
+const getDirectories = source =>
+  readdirSync(source).map(name => join(source, name));
+
+let directory = isDirectory(config.basePath);
+let directories = [];
+
+if(directory){
+  let paths = getDirectories(config.basePath);
+  for(let i=0;i< paths.length; i++){
+    directories.push(`\t'${paths[i]}',`);
+  }
+}else{
+  console.log('Please provide a directory');
+}
+directories.unshift("const paths=[");
+directories.push("]");
+
+writeFile(config.outputPath, directories.join("\n"), function(err) {
+  if(err) {
+      return console.log(err);
+  }
+
+  console.log("The file was saved!");
+}); 
